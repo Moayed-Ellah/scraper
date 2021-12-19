3987ca241b359672b85a80cdd8aa37562712fece
Moayed <96206184+Moayed-Ellah@users.noreply.github.com>
2021-12-19 12:19:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+# imports
+
 import requests
 from bs4 import BeautifulSoup
 
+# defining input
 
 link = input("input the website link here: ")
-
 page = requests.get(link)
 
 
-
+# soup scraping the content in all h1 divs
 soup = BeautifulSoup(page.content, "html.parser")
 element = soup.find("h1")
 output = print(element.text)
