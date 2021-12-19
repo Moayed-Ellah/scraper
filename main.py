405af946d50ba95b0b2016bb7f59e042e42c20be
Moayed <96206184+Moayed-Ellah@users.noreply.github.com>
2021-12-19 12:09:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-from collections import Counter
 
-URL = "https://moayedellah.tk"
-page = requests.get(URL)
+
+link = input("input the website link here: ")
+
+page = requests.get(link)
+
+
 
 soup = BeautifulSoup(page.content, "html.parser")
-job_elements = soup.find("html")
-output = print(job_elements.text)
-split_it = output.split()
-  
-
-Counter = Counter(split_it)
-most_occur = Counter.most_common(4)
-  
-print(most_occur)
+element = soup.find("h1")
+output = print(element.text)
